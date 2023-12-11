7fab636b45dbd6cf6212149709a29bde15f59a81
Mark <tka4mark@gmail.com>
2023-12-11 11:08:34 +0200

    UPD styles

--- src/components/Friends/friends.jsx
+++ src/components/Friends/friends.jsx
@@ -1,17 +1,18 @@
 import css from './Friends.module.css'
 
-export const FriendList = ({ friends }) => {
-    return (
-        <ul className={css.friend_list}>
-            {Array.isArray(friends) && friends.map(friend => {
-                return (
-                    <li className={css.friend_item} key={friend.id}>
-                        <span style={{backgroundColor: friend.isOnline ? 'green' : 'red'}} className={css.friend_status}>{friend.isOnline}</span>
-                        <img className={css.friend_avatar} src={friend.avatar} alt="User avatar" width="48" />
-                        <p className={css.friend_name}>{friend.name}</p>
-                    </li>
-                )
-            })}
-    </ul>
-)
-}
+export const FriendList = ({ friends }) => (
+    <ul className={css.friend_list}>
+    {friends.map(friend => (
+      <FriendListItem key={friend.id} {...friend} />
+    ))}
+  </ul>
+);
+
+
+const FriendListItem = ({ avatar, name, isOnline, id }) => (
+    <li className={css.friend_item} key={id}>
+        <span style={{ backgroundColor: isOnline ? 'green' : 'red' }} className={css.friend_status}>{isOnline}</span>
+        <img className={css.friend_avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.friend_name}>{name}</p>
+    </li>
+);
--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,10 +1,5 @@
 import css from './Statistics.module.css'
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+import { getRandomHexColor } from '../helper.js';
 
 export const Statistics = ({ stats, title = "" }) => {
     return (
